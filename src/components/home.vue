5423548f40c91ad98f1f7a3605c764b5ec7cdf64
reys <bastien.reys@neuf.fr>
2019-10-22 13:26:14 +0200

    ajout premiere page

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,7 @@
   <div class="bg-contact2" style="background-image: url('images/bg-01.jpg');">
 		<div class="container-contact2">
 			<div class="wrap-contact2">
-				<form class="contact2-form validate-form">
+				<form class="contact2-form validate-form" action="/game" method="POST">
 					<span class="contact2-form-title">
 						Connexion
 					</span>
@@ -20,7 +20,7 @@
 					<div class="container-contact2-form-btn">
 						<div class="wrap-contact2-form-btn">
 							<div class="contact2-form-bgbtn"></div>
-							<button class="contact2-form-btn">
+							<button class="contact2-form-btn" type="submit">
 								Connexion
 							</button>
 						</div>

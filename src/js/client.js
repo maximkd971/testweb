b026cde4c881ecad51b5380a36c69309fe14fbb7
CADET <maxi97115@gmail.com>
2019-11-04 18:02:28 +0100

    Système de connexion avec pseudo

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,30 @@
 var socket = null;
 
 var app = new Vue({
-	el :"#app",
+	el :"#content",
     data :{
-
+    	pseudo: '',
+    	emptyWord : false,
     },
 
     methods :{
-
+    	//Connexion de l'utilisateur
+    	//Envoyer juste le pseudo au serveur et stocker dans une variable de session
+    	session : function(){
+    		if (this.word != ''){
+    			this.emptyWord = false;
+	    		if (sessionStorage.getItem("autosave")) {
+				  // Restauration du contenu du champ
+				  sessionStorage.clear();
+				}
+				// Enregistrement de la saisie utilisateur dans le stockage de session
+				sessionStorage.setItem("autosave", this.pseudo);
+	    		socket.emit('connexion', this.pseudo);
+	    	}
+	    	else {
+	    		this.emptyWord = true;
+	    	}
+    	}
     },
 
     created : function(){

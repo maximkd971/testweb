b529736fca329f59104575360659c288d76adb6d
reys <bastien.reys@neuf.fr>
2019-11-04 20:43:50 +0100

    creation page salon

--- src/components/salons.vue
+++ src/components/salons.vue
@@ -1,18 +1,5 @@
 <template>
     <div>
-        <div style="text-align:center ; float:left ; height:100vh ; width:10%;">
-            Nous soutenir !
-        </div>
-
-        <div style="margin:auto ; text-align:center ; height:100vh ; width:70%">
-            <img src=../assets/image_bombe.png width="100%" height="35%">
-            <p>Créer un salon</p>
-            <p>Liste des salons </p> 
-        </div>
-
-        <div style="position:absolute ; top:0 ; right:0 ; text-align:center ; float:right ; height:100vh ; width:20%">
-            Amis 
-        </div>
     </div>
 </template>
 

ef4895b539eb6700dd784b9c4f368244d124f841
reys <bastien.reys@neuf.fr>
2019-11-06 17:01:22 +0100

    design du salon

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import home from './components/room.vue'
+import home from './components/salons.vue'
 
 export default {
   name: 'app',
--- src/components/salons.vue
+++ src/components/salons.vue
@@ -1,13 +1,33 @@
 <template>
     <div id="content">
-        <div style="margin:auto; text-align:center ; float:left ;  height:30%; width:15%;">
+        <div style="position:absolute ; margin:auto; text-align:center ; float:left ;  height:100%; width:15%; border-right-style: solid ; border-color: gray">
             <div>
                 <p><v-btn x-small v-on:click="quitter" rounded width="100%" height="55" color="#CDC5C4">Quitter</v-btn></p>
+            </div>
+            <br>
+            <div id="scores" style="margin: auto ; height:80vh ; width: 100% ; border-style: solid ; border-color: gray ; overflow:auto ; border-right-style: none ; border-bottom-style: none">
+ 
             </div>
         </div>
 
         <div style="margin:auto; text-align:center ;  height:100vh; width:70%;">
-            
+            <div style="margin: auto ; height:20% ; width: 100%"></div>
+            <div style="margin: auto ; height:60% ; width: 100%">
+                <div style="margin: auto ; height:100% ; width: 25% ; border-style: solid ; border-color: gray ; float: left">
+
+                </div>
+                <div style="margin: auto ; height:100% ; width: 25% ; border-style: solid ; border-color: gray ; float:left">
+
+                </div>
+                <div style="margin: auto ; height:100% ; width: 25% ; border-style: solid ; border-color: gray ; float:left">
+
+                </div>
+                <div style="margin: auto ; height:100% ; width: 25% ; border-style: solid ; border-color: gray ; float:left">
+
+                </div>
+            </div>
+            <div style="margin: auto ; height:20% ; width: 100%"></div>
+
         </div>
 
         <div style="position:absolute ; top:0 ; right:0 ; text-align:center ; float:right ; height:100% ; width:15% ; border-left-style: solid ; border-color: gray">

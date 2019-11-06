5c2d1264fcee8161c4d9a58e200ac1012cac3d22
Florian <>
2019-11-06 17:14:40 +0100

    fix merge1

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,22 @@
 <template>
-  <div id="app">
-    <home v-if="window.location.pathname === '/'"/>
-    <salons v-if="window.location.pathname === '/salons'"/>
-  </div>
+    <home/>
 </template>
 
 <script>
-import home from './components/home.vue'
-import salons from './components/salons.vue'
-    
+import home from './components/salons.vue'
+
 export default {
   name: 'app',
   components: {
-    home,
-    salons
-  },
-  data: function() {
-      return {
-          window: window
-      }
+    home
   }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
   margin-top: 60px;
 }
 </style>
